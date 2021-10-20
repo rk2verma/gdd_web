9d1e1db1922a7a6cadee8a55b0f74795f80c5c3d
rk2verma <rk2verma@gmail.com>
2021-10-20 17:30:19 +0530

    Add files via upload

---
+++ pages/modules/eng/toc0.js
@@ -0,0 +1,29 @@
+var tocDes = [{
+    "desc" : "1. Alphabet - Capital Letters ",
+    "ref" : "./eng/content/alphabet/Ch1.html?id=1"
+},
+{
+    "desc" : "2. Alphabet - Small Letters",
+    "ref" : "./eng/content/alphabet/Ch1.html?id=2"
+},
+{
+    "desc" : "3. Alphabet with phonics",
+    "ref" : "./eng/content/video/English Alphabets with Phonics.mp4"
+},
+{
+    "desc" : "4. Number Names 1 to 20",
+    "ref" : "./eng/content/video/Number Names 1-20.mp4"
+},
+{
+    "desc" : "5. Parts of Face ",
+    "ref" : "./eng/content/pof/Part_of_face.html"
+}/*,
+{
+    "desc" : "6. Parts of Body",
+    "ref" : "./eng/content/pob/Part_of_body.html"
+},
+{
+    "desc" : "Jason",
+    "ref" : "path"
+}*/];
+createTOC();
