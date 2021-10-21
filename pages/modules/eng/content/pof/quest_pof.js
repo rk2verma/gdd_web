312998c64ba67dc148f92ceac53b960fd5341c55
rk2verma <rk2verma@gmail.com>
2021-10-21 12:14:17 +0530

    alphabetAdded

---
+++ pages/modules/eng/content/pof/quest_pof.js
@@ -0,0 +1,26 @@
+var mainFilePath="./Part_of_face.html";
+var headTitle = "Parts of Face";
+var questions = [{
+    quest: "Which part do you smell with?",
+    quest1: "",
+    choices: ['Cheek', 'Nose', 'Elbow', 'Ankle'],
+    correctAnswer: 1
+  }, 
+  {
+    quest: "Which part do you listen with?",
+    quest1: "",
+    choices: ['Ear', 'Eye', 'Nose', 'Lips'],
+    correctAnswer: 0
+  }, 
+  {
+    quest: "Which part do you see with?",
+    quest1: "",
+    choices: ['Ear', 'Eyes', 'Nose', 'Cheek'],
+    correctAnswer: 1
+  }, 
+  {
+    quest: "Which part do you taste with?",
+    quest1: "",
+    choices: ['Cheek', 'Ear', 'Nose', 'Tongue'],
+    correctAnswer: 3
+  }];
